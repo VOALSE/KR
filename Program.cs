38bebcc1420bb533df6a45744e32de17070fbff4
Alexandr <sashkavoytov20@gmail.com>
2022-09-11 11:18:34 +0500

    Выполнил решение задачи

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] arrayOld = new string[4] {"hello", "2", "world", ":-)"};
+string[] arrayNew = new string[arrayOld.Length];
+
+void SearchingInArray(string[] arrayOld, string[] arrayNew)
+{
+    int temp = 0;
+    for (int i = 0; i < arrayOld.Length; i++)
+    {
+    if(arrayOld[i].Length <= 3)
+        {
+        arrayNew[temp] = arrayOld[i];
+        temp++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SearchingInArray(arrayOld, arrayNew);
+PrintArray(arrayNew);
